f97ebb6c8d2fce23291b9b877054e895ef4fe4d0
Mehmetkrks09 <mehmetkarakas415@gmail.com>
2021-06-13 23:41:51 +0300

    13.06.2021

---
+++ Hrms/src/main/java/kodlama/io/Hrms/api/Controllers/CityController.java
@@ -0,0 +1,42 @@
+package kodlama.io.Hrms.api.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.Hrms.Business.abstracts.CityService;
+import kodlama.io.Hrms.Business.abstracts.EmployeeService;
+import kodlama.io.Hrms.Core.Results.DataResult;
+import kodlama.io.Hrms.Core.Results.Result;
+import kodlama.io.Hrms.entities.concretes.City;
+import kodlama.io.Hrms.entities.concretes.Employee;
+
+@RestController
+@RequestMapping("/api/city")
+public class CityController {
+
+	private CityService cityService;
+
+	@Autowired
+	public CityController(CityService cityService) {
+		super();
+		this.cityService = cityService;
+	}
+
+	@PostMapping("/add")
+	Result add(@RequestBody City city) {
+		return this.cityService.add(city);
+	}
+
+	@GetMapping("getAll")
+	public DataResult<List<City>> getAll() {
+		return this.cityService.getAll();
+
+	}
+
+}
